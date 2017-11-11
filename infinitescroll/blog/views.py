70692b7c73de20ca89392c4db162ea248c359303
xabita <mutskxabaa@gmail.com>
2017-11-11 02:32:29 -0600

    Initial
    Initial files

---
+++ infinitescroll/blog/views.py
@@ -0,0 +1,18 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
+
+# Create your views here.
+
+
+
+def home(request):
+    numbers_list = range(1, 1000)
+    page = request.GET.get('page', 1)
+    paginator = Paginator(numbers_list, 20)
+    try:
+        numbers = paginator.page(page)
+    except PageNotAnInteger:
+        numbers = paginator.page(1)
+    except EmptyPage:
+        numbers = paginator.page(paginator.num_pages)
+    return render(request, 'blog/home.html', {'numbers': numbers})
